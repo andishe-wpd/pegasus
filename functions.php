f6804b9e20d1cf83e9a321d5c89041efe6ad1633
andishe abasian <rainbowboxdash@gmail.com>
2021-07-20 00:30:21 +0430

    first commit

---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php
+function styleandscripts(){
+    wp_enqueue_script('jsscriptsforthesakeofgod',get_theme_file_uri('/js/scripts-bundled.js'),NULL,'1.0',true);
+    wp_enqueue_style('refertomaincss',get_stylesheet_uri(  ));
+    wp_enqueue_style('fontawesome','//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+    wp_enqueue_style('googlefont','//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+}
+add_action('wp_enqueue_scripts','styleandscripts');
+function titlegenerator(){
+    add_theme_support('title-tag');
+}
+add_action('after_setup_theme','titlegenerator');
