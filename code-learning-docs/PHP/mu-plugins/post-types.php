28438d95a689d23977bffdb80881e926880488a8
andishe abasian <rainbowboxdash@gmail.com>
2021-07-26 21:40:43 +0430

    started order custom query by custom field value

--- code-learning-docs/PHP/mu-plugins/post-types.php
+++ code-learning-docs/PHP/mu-plugins/post-types.php
@@ -22,3 +22,26 @@ add_action('init','function_that_register_new_post_type')
 //we know this custom post types now lives on cyrrent theme files 
 // => the importance of using MU-plugins
 ?>
+
+
+<?php
+
+function university_post_types() {
+  register_post_type('event', array(
+    'show_in_rest' => true,
+    'supports' => array('title', 'editor', 'excerpt'), //you can use custom fields here
+    'rewrite' => array('slug' => 'events'),
+    'has_archive' => true,
+    'public' => true,
+    'labels' => array(
+      'name' => 'Events',
+      'add_new_item' => 'Add New Event',
+      'edit_item' => 'Edit Event',
+      'all_items' => 'All Events',
+      'singular_name' => 'Event'
+    ),
+    'menu_icon' => 'dashicons-calendar'
+  ));
+}
+
+add_action('init', 'university_post_types');
